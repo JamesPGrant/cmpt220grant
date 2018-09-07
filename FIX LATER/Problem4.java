e0c6f0709db2439f71a0885c50706c1dba118ffe
JamesPGrant <42850394+JamesPGrant@users.noreply.github.com>
2018-09-07 18:16:24 -0400

    Update Problem4.java

--- FIX LATER/Problem4.java
+++ FIX LATER/Problem4.java
@@ -1,5 +1,11 @@
 package problem4;
 
 public class Problem4 {
-
-}
+  public static void main(String[]args){
+    double light = 300000000
+    dobule result = 300000000*300000000*mass
+    Scanner input = new Scanner(System.in);
+    system.out.println("Enter the mass of the object:")
+    double mass = input.nextDouble();
+    
+ }
